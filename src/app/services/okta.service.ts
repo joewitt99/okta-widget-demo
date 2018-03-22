fd099408cb6c8d8ebb8863c8f994ab131c82dbfe
Joe Witt <joewitt99@gmail.com>
2018-03-21 21:47:25 -0400

    removed environment files

--- src/app/services/okta.service.ts
+++ src/app/services/okta.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 import * as OktaSignIn from '@okta/okta-signin-widget';
 
 @Injectable()
@@ -6,22 +7,7 @@ export class Okta {
   widget;
 
   constructor() {
-    this.widget = new OktaSignIn({
-      baseUrl: 'https://ienergycloud.oktapreview.com',
-      clientId: '0oaebvdradAWPRYyo0h7',
-      redirectUri: 'http://localhost:4200/callback',
-      authParams: {
-        issuer: 'https://ienergycloud.oktapreview.com/oauth2/auseblc3wv9PSUKv10h7',
-        authorizeUrl: 'https://ienergycloud.oktapreview.com/oauth2/auseblc3wv9PSUKv10h7/v1/authorize',
-        responseType: ['id_token', 'token'],
-        scopes: ['openid', 'profile']
-      },
-      features: {
-        rememberMe: true,
-        smsRecovery: true,
-        selfServiceUnlock: true
-      }
-    });
+    this.widget = new OktaSignIn(environment.oktaWidgetConfig);
   }
 
   /**
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,21 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  oktaWidgetConfig: {
+    baseUrl: 'https://ienergycloud.oktapreview.com',
+      clientId: '0oaebvdradAWPRYyo0h7',
+    redirectUri: 'http://localhost:4200/callback',
+    authParams: {
+    issuer: 'https://ienergycloud.oktapreview.com/oauth2/auseblc3wv9PSUKv10h7',
+      authorizeUrl: 'https://ienergycloud.oktapreview.com/oauth2/auseblc3wv9PSUKv10h7/v1/authorize',
+      responseType: ['id_token', 'token'],
+      scopes: ['openid', 'profile']
+  },
+    features: {
+      rememberMe: true,
+        smsRecovery: true,
+        selfServiceUnlock: true
+    }
+  }
 };
